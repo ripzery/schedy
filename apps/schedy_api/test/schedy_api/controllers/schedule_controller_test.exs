999462aa3533fc59e2384b2aba4d054b2301be88
euro <phuchit@omise.co>
2019-03-03 14:51:00 +0700

    :hammer: Add test for `schedule.get`

--- apps/schedy_api/test/schedy_api/controllers/schedule_controller_test.exs
+++ apps/schedy_api/test/schedy_api/controllers/schedule_controller_test.exs
@@ -2,8 +2,17 @@ defmodule SchedyAPI.ScheduleControllerTest do
   use SchedyAPI.ConnCase
   import SchedyAPI.ConnCase
 
+  test "GET /api/schedule.get" do
+    response = post_request("/api/schedule.add", %{from: "07:00", to: "14:00"})
+    %{"id" => id} = response["data"]["data"]
+
+    body = get_request("/api/schedule.get?id=#{id}")
+
+    assert body["data"]["data"]["id"] == id
+    assert body["status"] == "ok"
+  end
+
   test "GET /api/schedule.all" do
-    # Prepare
     post_request("/api/schedule.add", %{from: "07:00", to: "14:00"})
     post_request("/api/schedule.add", %{from: "08:00", to: "20:00"})
     post_request("/api/schedule.add", %{from: "09:00", to: "23:00"})
@@ -16,18 +25,23 @@ defmodule SchedyAPI.ScheduleControllerTest do
       %{"from" => "09:00", "object" => "schedule", "to" => "23:00"}
     ]
 
-    data = Enum.map(body["data"], fn schedule -> Map.drop(schedule, ["inserted_at", "id"]) end)
+    data =
+      Enum.map(body["data"]["data"], fn schedule -> Map.drop(schedule, ["inserted_at", "id"]) end)
 
+    assert body["status"] == "ok"
     assert data == expected_data
   end
 
   test "POST /api/schedule.add" do
     response = post_request("/api/schedule.add", %{from: "07:00", to: "14:00"})
     assert response["status"] == "ok"
+    assert %{"from" => "07:00", "to" => "14:00"} = response["data"]["data"]
   end
 
   test "POST /api/schedule.delete" do
-    %{"id" => id} = post_request("/api/schedule.add", %{from: "07:00", to: "14:00"})
+    %{"data" => data} = post_request("/api/schedule.add", %{from: "07:00", to: "14:00"})
+    id = data["data"]["id"]
+
     response = post_request("/api/schedule.delete", %{id: id})
 
     assert response["status"] == "ok"
